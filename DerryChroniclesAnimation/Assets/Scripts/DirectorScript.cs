5bee997728c05ce0d7d6131dccfc407d5d88f1f8
Iván Almanza <ivan.almanza.g@gmail.com>
2019-05-16 08:24:54 -0500

    Cab Animation
    Cab and Director Script

---
+++ DerryChroniclesAnimation/Assets/Scripts/DirectorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectorScript : MonoBehaviour
+{
+    public Animator CabAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+      StartCoroutine(CabStop());
+      StartCoroutine(CabGo());
+    }
+
+    IEnumerator CabStop()
+    {
+      yield return new WaitForSeconds(7f);
+      CabAnim.SetBool("IsGoing", true);
+    }
+
+    IEnumerator CabGo()
+    {
+      yield return new WaitForSeconds(9.5f);
+      CabAnim.SetBool("IsGoing", false);
+    }
+
+}//class
