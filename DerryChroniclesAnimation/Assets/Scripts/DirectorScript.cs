ebe8712c594458bdc902c94a2dd3fe0e95964e92
Iván Almanza <ivan.almanza.g@gmail.com>
2019-05-19 09:31:26 -0500

    24 seconds

--- DerryChroniclesAnimation/Assets/Scripts/DirectorScript.cs
+++ DerryChroniclesAnimation/Assets/Scripts/DirectorScript.cs
@@ -6,12 +6,17 @@ public class DirectorScript : MonoBehaviour
 {
     public Animator CabAnim;
     public Animator FrankAnim;
+    public Animator DrugStoreAnim;
+    public SpriteRenderer DrugStoreRender;
     // Start is called before the first frame update
     void Start()
     {
       StartCoroutine(CabStop());
       StartCoroutine(CabGo());
       StartCoroutine(FrankWalk());
+      StartCoroutine(FrankGetIn());
+      StartCoroutine(DrugStoreOpen());
+      StartCoroutine(DrugStoreClose());
     }
 
     IEnumerator CabStop()
@@ -30,6 +35,25 @@ public class DirectorScript : MonoBehaviour
     {
       yield return new WaitForSeconds(14.6f);
       FrankAnim.SetTrigger("Walk");
+    }
+
+    IEnumerator FrankGetIn()
+    {
+      yield return new WaitForSeconds(20.5f);
+      FrankAnim.SetBool("GetIn", true);
+    }
+
+    IEnumerator DrugStoreOpen()
+    {
+      yield return new WaitForSeconds(22f);
+      DrugStoreAnim.SetBool("DrugStoreOpen", true);
+    }
+
+    IEnumerator DrugStoreClose()
+    {
+      yield return new WaitForSeconds(23f);
+      DrugStoreAnim.SetBool("DrugStoreOpen", false);
+      DrugStoreRender.sortingOrder = 5;
     }
 
 }//class
